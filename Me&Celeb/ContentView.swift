bb233bf914f8287416a02f17e2b857fbd87af199
underTheBlueSun <wind7129@naver.com>
2021-11-23 11:00:14 +0900

    Initial Commit

---
+++ Me&Celeb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Me&Celeb
+//
+//  Created by macbook on 2021/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Me&Celeb/Me_CelebApp.swift
@@ -0,0 +1,17 @@
+//
+//  Me_CelebApp.swift
+//  Me&Celeb
+//
+//  Created by macbook on 2021/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Me_CelebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
